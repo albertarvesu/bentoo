6c95274cf771a50ae63c9b4a7435642f13959c40
Albert Arvesu <albertarvesu@gmail.com>
2012-05-07 17:47:11 +0800

    Renamed application + Home Page Controller

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,4 +1,4 @@
-Bentoo::Application.configure do
+Beento::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
 
   # In the development environment your application's code is reloaded on
--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Bentoo::Application.config.secret_token = '3f1cff568ea99100490d6a36979b2ef5d3d5b0eb9bf02934459dca28d5ca9972ac1df81ee72a44251bb78ba9987540d2142c065ce76f2af71a48677609b59294'
+Beento::Application.config.secret_token = '3f1cff568ea99100490d6a36979b2ef5d3d5b0eb9bf02934459dca28d5ca9972ac1df81ee72a44251bb78ba9987540d2142c065ce76f2af71a48677609b59294'
