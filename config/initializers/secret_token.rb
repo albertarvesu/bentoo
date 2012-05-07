5bf7b006eafdb6c0883d54d6830d5abadb13e971
Albert Arvesu <albertarvesu@gmail.com>
2012-05-07 15:53:47 +0800

    Initial commit of the rails framework.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bentoo::Application.config.secret_token = '3f1cff568ea99100490d6a36979b2ef5d3d5b0eb9bf02934459dca28d5ca9972ac1df81ee72a44251bb78ba9987540d2142c065ce76f2af71a48677609b59294'
